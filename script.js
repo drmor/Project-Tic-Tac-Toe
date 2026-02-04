5aac3aa4000d0a3e2309da6b67b5c4e8c963c31c
Miki D <69644623+drmor@users.noreply.github.com.>
2026-02-04 14:57:26 +0500

    refactor previous code, now players changes every turn

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameBoard = (function (){
-    const board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    let board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     const getBoard = () =>{
     for (let i = 0; i < 9; i += 3){
-        console.log(" " + board[i] + " | " + board[i + 1] + " | "+ board[i + 2])
+        console.log(" " + board[i] + " | " + board[i + 1] + " | " + board[i + 2])
             if (i < 6) {
                 console.log("---+---+---");
             };
@@ -12,13 +12,10 @@ const gameBoard = (function (){
         if (index >= 0 && index <= 8){
             board[index] = mark;
         }
-    }
-    for (let n = 0; n < 3; n++){
-        setMark(prompt("index"), prompt("mark"))
-        getBoard();
-        if (n < 2){
-            console.log("next player turn")
-        }
+    };
+    return {
+        getBoard,
+        setMark,
     };
 })();
 
@@ -26,9 +23,23 @@ function createPlayer(name, mark){
     return {name, mark};
 };
 
-function game(){
-    const p1 = createPlayer("p1", "X");
-    const p2 = createPlayer("p2", "O");
+const gameController = (function(){
+    let players = [];
+    let activePlayer = 0;
+    players.push(p1 = createPlayer("p1", "X"));
+    players.push(p2 = createPlayer("p2", "O"));
 
-    
-}
+    const changePlayer = () =>{
+        activePlayer = activePlayer === 0 ? 1 : 0;
+    };
+    const getMark = () =>{
+        gameBoard.setMark(`${prompt(`index for ${players[activePlayer].name}`)}`, players[activePlayer].mark);
+    };
+    for (i = 0; i < 3; i++){
+        getMark();
+        changePlayer();
+        gameBoard.getBoard();
+        console.log(" ");
+    };
+    return{changePlayer, getMark}
+})();
